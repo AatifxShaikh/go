55c751949e18eca1c9843e37e5f1714e165fdff4
AatifxShaikh <shaatif934@gmail.com>
2025-01-22 11:46:39 +0530

    Done with hello world in Go

--- hello.go
+++ hello.go
@@ -2,9 +2,36 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello" + " " + name
+const hello = "Hello, "
+const Spanish = "Spanish"
+const helloForSpanish = "Hola, "
+const French = "French"
+const helloForFrench = "Bonjour, "
+const Japanese = "Japanese"
+const helloForJapanese = "Konnichiwa, "
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	// if language == Spanish {
+	// 	return helloForSpanish + name
+	// }
+	// if language == French {
+	// 	return helloForFrench + name
+	// }
+	//refactoring and adding switch
+	prefix := hello
+	switch language {
+	case Spanish:
+		prefix = helloForSpanish
+	case French:
+		prefix = helloForFrench
+	case Japanese:
+		prefix = helloForJapanese
+	}
+	return prefix + name
 }
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "English"))
 }
